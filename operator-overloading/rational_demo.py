507aac9369c21136fac17927fdcc55683aad7769
Michael Saunby <mike@saunby.net>
2021-11-17 15:16:31 +0000

    exercise

---
+++ operator-overloading/rational_demo.py
@@ -0,0 +1,18 @@
+""" Rational numbers.
+
+Michael Saunby. November 2021.
+
+This module is an exercise in the use of operator overloading.
+The code works, but is incomplete.
+"""
+
+from rational import Rational
+
+a = Rational(1, 2)
+b = Rational(1, 8)
+print("Add two rational numbers")
+print(f"{a} + {b} = {a+b}")
+
+print()
+print("Subtract one rational number from another")
+print(f"{a} - {b} = {a-b}")
